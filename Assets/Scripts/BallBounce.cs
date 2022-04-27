7098cc701177355b8e53e4ce2be8514893db8112
Ävelin <a.pantigoso@hotmail.com>
2022-04-27 15:24:44 +0200

    Two rock prefab for use as obsticles added, with bounciness

--- Assets/Scripts/BallBounce.cs
+++ Assets/Scripts/BallBounce.cs
@@ -7,15 +7,19 @@ public class BallBounce : MonoBehaviour
     // Start is called before the first frame update
 
     public Vector3 force;
+    public Vector3 rotation;
+    private Rigidbody rigid;
+    private Vector3 rand;
     void Start()
     {
-        Rigidbody rigid = GetComponent<Rigidbody>();
-        rigid.AddForce(force, ForceMode.Impulse);
+       rigid = GetComponent<Rigidbody>();
+       rand = new Vector3(UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigid.AddForce(Vector3.Scale(force, rand), ForceMode.Impulse);
+        transform.Rotate(rotation);
     }
 }
